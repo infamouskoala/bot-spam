9a376b4f7a9de4800790662f8fbc8646d46dc354
Knfamous Koala <75808182+Koala-3k@users.noreply.github.com>
2023-01-01 10:50:00 +0530

    Add files via upload

---
+++ bot-spam.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext.commands import Bot
+
+koala = Bot("$", intents=discord.Intents.all())
+x = int(input("enter channel id here: "))
+msg = input("enter bot spam message: ")
+tkn = input("enter bot token: ")
+times = int(input("enter the amount of messages: "))
+
+@koala.event
+async def on_ready():
+    for i in range(times):
+            await koala.get_channel(x).send(msg)
+
+koala.run(tkn)
